f4c15bbb31e38a781812856390d96c3ceb052c0e
Yukishige Nakajo <nakajo@chaintope.com>
2023-05-12 03:50:29 +0900

    Fixed prettier warnings.

--- frontend/src/app/transaction/transaction.module.ts
+++ frontend/src/app/transaction/transaction.module.ts
@@ -8,7 +8,7 @@ import { SharedPipeModule } from '../modules/sharePipe.module';
 import { TransactionPageRoutingModule } from './transaction-routing.module';
 import { TransactionPage } from './transaction.page';
 import { TransactionRawdataPage } from '../transaction-rawdata/transaction-rawdata.page';
-import {TransactionRawdataPageModule} from '../transaction-rawdata/transaction-rawdata.module';
+import { TransactionRawdataPageModule } from '../transaction-rawdata/transaction-rawdata.module';
 
 @NgModule({
   imports: [
