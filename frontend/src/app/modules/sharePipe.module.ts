03260beefabb124474f87d4b84526504b1aecbb0
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2023-08-06 18:45:34 +0900

    Format date

--- frontend/src/app/modules/sharePipe.module.ts
+++ frontend/src/app/modules/sharePipe.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MomentFromNowPipe } from '../pipes/moment-from-now.pipe';
 import { AsTpcPipe } from '../pipes/as-tpc.pipe';
+import { DateFormatPipe } from '../pipes/date-format.pipe';
 @NgModule({
-  declarations: [MomentFromNowPipe, AsTpcPipe],
+  declarations: [MomentFromNowPipe, AsTpcPipe, DateFormatPipe],
   imports: [CommonModule],
-  exports: [MomentFromNowPipe, AsTpcPipe]
+  exports: [MomentFromNowPipe, AsTpcPipe, DateFormatPipe]
 })
 export class SharedPipeModule {}
---
+++ frontend/src/app/pipes/date-format.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
+import * as timezone from 'moment-timezone';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+@Pipe({
+  name: 'dateFormat',
+  pure: false
+})
+export class DateFormatPipe implements PipeTransform {
+  transform(time: any, ...args: any[]): any {
+    if (!time) {
+      return 'N/A';
+    }
+    return moment
+      .unix(time)
+      .tz(timezone.tz.guess())
+      .format('X (MMM DD, yyyy hh:mm:ss z)');
+  }
+}
