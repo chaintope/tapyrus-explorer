929b54fe9726bce65efdc8f9c84f53bf12e19901
Kohei Taniguchi <kohei@chaintope.com>
2023-08-16 14:51:07 +0900

    Fix nginx docker image building failure

--- frontend/src/app/backend.service.ts
+++ frontend/src/app/backend.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { ConfigService } from './config.service';
 
 @Injectable({ providedIn: 'root' })
+@NgModule()
 export class BackendService {
   backendUrl = 'http://localhost:3001';
   constructor(private http: HttpClient, private configService: ConfigService) {
