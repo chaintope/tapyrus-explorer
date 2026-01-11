824bbfa8e44c339b3f87f4b68b1d1670549c83ff
azuchi <azuchi@chaintope.com>
2026-01-11 17:31:41 +0900

    Fix node-fetch ESM compatibility issue and add module load test
      - Downgrade node-fetch from v3.x to v2.7.0
        node-fetch v3.x is ESM-only and incompatible with CommonJS require()
      - Add rest.spec.js to verify rest module can be loaded
        This test will fail in CI if node-fetch is updated to v3.x+

---
+++ backend/test/libs/rest.spec.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+
+describe('rest module', () => {
+  describe('require', () => {
+    it('should be able to require rest module (checks node-fetch CommonJS compatibility)', () => {
+      // This test verifies that the rest module can be loaded.
+      // It will fail if node-fetch is ESM-only (v3.x+) because require() won't work.
+      const rest = require('../../libs/rest');
+
+      assert.ok(rest.address, 'address should be defined');
+      assert.ok(rest.transaction, 'transaction should be defined');
+      assert.ok(rest.block, 'block should be defined');
+      assert.ok(rest.color, 'color should be defined');
+      assert.ok(rest.mempool, 'mempool should be defined');
+
+      // Verify that functions are exported correctly
+      assert.strictEqual(typeof rest.address.stats, 'function');
+      assert.strictEqual(typeof rest.address.txs, 'function');
+      assert.strictEqual(typeof rest.transaction.get, 'function');
+      assert.strictEqual(typeof rest.transaction.raw, 'function');
+      assert.strictEqual(typeof rest.block.get, 'function');
+      assert.strictEqual(typeof rest.block.list, 'function');
+      assert.strictEqual(typeof rest.block.height, 'function');
+      assert.strictEqual(typeof rest.block.tip.height, 'function');
+    });
+  });
+});
