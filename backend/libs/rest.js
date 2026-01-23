2139c27d1a0f92b747342255746751e630f82208
azuchi <azuchi@chaintope.com>
2026-01-23 13:22:33 +0900

    Refactoring redundant processes

--- backend/libs/rest.js
+++ backend/libs/rest.js
@@ -1,204 +1,102 @@
 const config = require('./config');
 
 const baseUrl = `${config.rest.schema}://${config.rest.host}:${config.rest.port}`;
-const address = {
-  stats: async address => {
-    const url = `${baseUrl}/address/${address}`;
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
-  },
-  txs: async (address, lastSeenTxid) => {
-    let url;
-    if (lastSeenTxid) {
-      url = `${baseUrl}/address/${address}/txs/chain/${lastSeenTxid}`;
-    } else {
-      url = `${baseUrl}/address/${address}/txs`;
-    }
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
-  },
-  utxo: async address => {
-    const url = `${baseUrl}/address/${address}/utxo`;
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
+
+// Helper functions
+const fetchJson = async url => {
+  const response = await fetch(url);
+  if (response.ok) {
+    return response.json();
   }
+  throw new Error(`failed to fetch API ${url}`);
+};
+
+const fetchJsonOr404 = async url => {
+  const response = await fetch(url);
+  if (response.ok) {
+    return response.json();
+  }
+  if (response.status === 404) {
+    return null;
+  }
+  throw new Error(`failed to fetch API ${url}`);
+};
+
+const fetchTextOr404 = async url => {
+  const response = await fetch(url);
+  if (response.ok) {
+    return response.text();
+  }
+  if (response.status === 404) {
+    return null;
+  }
+  throw new Error(`failed to fetch API ${url}`);
+};
+
+const address = {
+  stats: async address => fetchJson(`${baseUrl}/address/${address}`),
+  txs: async (address, lastSeenTxid) => {
+    const url = lastSeenTxid
+      ? `${baseUrl}/address/${address}/txs/chain/${lastSeenTxid}`
+      : `${baseUrl}/address/${address}/txs`;
+    return fetchJson(url);
+  },
+  utxo: async address => fetchJson(`${baseUrl}/address/${address}/utxo`)
 };
 
 const transaction = {
-  get: async txid => {
-    const url = `${baseUrl}/tx/${txid}`;
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.json();
-    } else if (response.status === 404) {
-      return null;
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
-  },
-  raw: async txid => {
-    const url = `${baseUrl}/tx/${txid}/hex`;
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.text();
-    } else if (response.status === 404) {
-      return null;
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
-  }
+  get: async txid => fetchJsonOr404(`${baseUrl}/tx/${txid}`),
+  raw: async txid => fetchTextOr404(`${baseUrl}/tx/${txid}/hex`)
 };
 
 const block = {
-  get: async blockHash => {
-    const url = `${baseUrl}/block/${blockHash}`;
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.json();
-    } else if (response.status === 404) {
-      return null;
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
-  },
-  list: async startIndex => {
-    const url = `${baseUrl}/blocks/${startIndex}`;
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
-  },
-  height: async height => {
-    const url = `${baseUrl}/block-height/${height}`;
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.text();
-    } else if (response.status === 404) {
-      return null;
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
-  },
-  raw: async blockHash => {
-    const url = `${baseUrl}/block/${blockHash}/header`;
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.text();
-    } else if (response.status === 404) {
-      return null;
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
-  },
-  status: async blockHash => {
-    const url = `${baseUrl}/block/${blockHash}/status`;
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.json();
-    } else if (response.status === 404) {
-      return null;
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
-  },
+  get: async blockHash => fetchJsonOr404(`${baseUrl}/block/${blockHash}`),
+  list: async startIndex => fetchJson(`${baseUrl}/blocks/${startIndex}`),
+  height: async height => fetchTextOr404(`${baseUrl}/block-height/${height}`),
+  raw: async blockHash =>
+    fetchTextOr404(`${baseUrl}/block/${blockHash}/header`),
+  status: async blockHash =>
+    fetchJsonOr404(`${baseUrl}/block/${blockHash}/status`),
   tip: {
-    height: async () => {
-      const url = `${baseUrl}/blocks/tip/height`;
-      const response = await fetch(url);
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error(`failed to fetch API ${url}`);
-      }
-    }
+    height: async () => fetchJson(`${baseUrl}/blocks/tip/height`)
   },
   txs: async (blockHash, startIndex) => {
-    let url;
-    if (startIndex) {
-      url = `${baseUrl}/block/${blockHash}/txs/${startIndex}`;
-    } else {
-      url = `${baseUrl}/block/${blockHash}/txs`;
-    }
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
+    const url = startIndex
+      ? `${baseUrl}/block/${blockHash}/txs/${startIndex}`
+      : `${baseUrl}/block/${blockHash}/txs`;
+    return fetchJson(url);
   }
 };
 
 const color = {
   list: async lastSeenColorId => {
-    let url;
-    if (lastSeenColorId) {
-      url = `${baseUrl}/colors/${lastSeenColorId}`;
-    } else {
-      url = `${baseUrl}/colors`;
-    }
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
+    const url = lastSeenColorId
+      ? `${baseUrl}/colors/${lastSeenColorId}`
+      : `${baseUrl}/colors`;
+    return fetchJson(url);
   },
-  get: async colorId => {
-    const url = `${baseUrl}/color/${colorId}`;
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
-  },
+  get: async colorId => fetchJson(`${baseUrl}/color/${colorId}`),
   txs: async (colorId, lastSeenTxid) => {
-    let url;
-    if (lastSeenTxid) {
-      url = `${baseUrl}/color/${colorId}/txs/chain/${lastSeenTxid}`;
-    } else {
-      url = `${baseUrl}/color/${colorId}/txs`;
-    }
-    const response = await fetch(url);
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
-    }
+    const url = lastSeenTxid
+      ? `${baseUrl}/color/${colorId}/txs/chain/${lastSeenTxid}`
+      : `${baseUrl}/color/${colorId}/txs`;
+    return fetchJson(url);
   }
 };
 
 const mempool = {
   list: async startIndex => {
-    let url;
-    if (startIndex) {
-      url = `${baseUrl}/mempool/txs/${startIndex}`;
-    } else {
-      url = `${baseUrl}/mempool/txs`;
-    }
+    const url = startIndex
+      ? `${baseUrl}/mempool/txs/${startIndex}`
+      : `${baseUrl}/mempool/txs`;
     const response = await fetch(url);
     if (response.ok) {
       return {
         count: response.headers.get('X-Total-Results'),
         txs: await response.json()
       };
-    } else {
-      throw new Error(`failed to fetch API ${url}`);
     }
+    throw new Error(`failed to fetch API ${url}`);
   }
 };
 
