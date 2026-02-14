92c5562b6f32a17add7a6c9e91c03b0ac234e383
azuchi <azuchi@chaintope.com>
2026-02-14 16:36:37 +0900

    Refactoring to use metadata fetch function in tapyrusjs-lib

--- backend/libs/config.js
+++ backend/libs/config.js
@@ -5,11 +5,6 @@ const defaultConfig = {
     schema: process.env.ELECTRS_REST_SCHEMA || 'http',
     host: process.env.ELECTRS_REST_HOST || 'localhost',
     port: process.env.ELECTRS_REST_PORT || 3000
-  },
-  tokenRegistry: {
-    baseUrl:
-      process.env.TOKEN_REGISTRY_URL ||
-      'https://chaintope.github.io/tapyrus-token-registry/tokens'
   }
 };
 
